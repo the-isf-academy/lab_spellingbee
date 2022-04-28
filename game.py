3aec9b706235dbf53de20d4aa38e6ec0ff4040e5
Emma <eqbrown@isf.edu.hk>
2022-04-28 08:16:08 +0800

    minor fixes to formatting

--- game.py
+++ game.py
@@ -1,27 +1,37 @@
 from spellingbee import SpellingBee
 from view import TerminalView
 
-spelling_bee = SpellingBee()
-view = TerminalView()
-
-
 def game():
+
+    spelling_bee = SpellingBee()
+    view = TerminalView()
+
+    view.mystery_method_0()
+
     view.mystery_method_1(spelling_bee.letters, spelling_bee.keyletter)
+
     while True:
+
         user_guess = view.mystery_method_2()
+
         for letter in user_guess:
             if letter not in spelling_bee.letters:
                 break
 
         if letter not in spelling_bee.letters:
             view.mystery_method_3()
+
         elif spelling_bee.keyletter not in user_guess:
             view.mystery_method_4()
+
         elif user_guess in spelling_bee.guessedwords:
             view.mystery_method_5()
+            
         elif user_guess in spelling_bee.wordlist:
             spelling_bee.correct_guess(user_guess)
             view.mystery_method_6("mystery_value")
+
         else:
             view.mystery_method_7()
+
 game()
--- view.py
+++ view.py
@@ -1,11 +1,16 @@
 class TerminalView:
+
+    def mystery_method_0(self):
+        print("\n---Welcome to Spelling Bee---")
+
     def mystery_method_1(self, letters, keyletter):
-        print("""\nRULES: You can use any of these letters: {}
-        You must you use the letter {} \n""".format(letters, keyletter))
+        print("\n[RULES]")
+        print("You can use any of these letters: {}".format(letters))
+        print("You must you use the letter {}".format(keyletter))
 
     def mystery_method_2(self):
         print("\n---Mystery Method 2---")
-        guess = input("> ")
+        guess = input(" > ")
         return guess.upper()
 
     def mystery_method_3(self):
